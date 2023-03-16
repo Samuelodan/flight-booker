9790a94dfdbdee57d9f5cc6486f703801ddb7955
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-03-17 00:01:34 +0100

    feat(seeds): write code to create Airports

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+airport_names_and_codes = [
+  %w[Manhattan MHK], %w[Minneapolis MSP], %w[New Orleans MSY],
+  %w[New York NYC], %w[San Francisco SFO], %w[Washington DC WAS]
+]
+
+airport_names_and_codes.each do |item |
+  Airport.create!(name: item.first, code: item.last)
+end
+puts 'created airports'
