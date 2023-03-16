1956f5e8f0f3bad5cdd83d0ac414bd249f930e7e
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-03-16 22:56:00 +0100

    feat(db): add `duration` column to `flights` table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_154346) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_214625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_154346) do
     t.datetime "updated_at", null: false
     t.bigint "departure_airport_id", null: false
     t.bigint "arrival_airport_id", null: false
+    t.bigint "duration"
     t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
