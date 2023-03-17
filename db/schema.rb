760c5ef6b3bff3feb1cdb66b927e3a3081ee6603
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-03-17 23:25:48 +0100

    feat(schema): remove `time` column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_17_220355) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_17_222312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_17_220355) do
 
   create_table "flights", force: :cascade do |t|
     t.datetime "date", precision: nil
-    t.time "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "departure_airport_id", null: false
