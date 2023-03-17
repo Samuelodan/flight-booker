80ffea9e3c9aaaf27e4c4e126a6fc58070238a6a
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-03-17 23:13:16 +0100

    feat(schema): change `date` column to `datetime` type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_214625) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_17_220355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_214625) do
   end
 
   create_table "flights", force: :cascade do |t|
-    t.date "date"
+    t.datetime "date", precision: nil
     t.time "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
