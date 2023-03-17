4d077e1f4d48f9e4858dd2273ed9112d943e3b86
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-03-18 00:24:57 +0100

    feat(seeds): write code to create Flights
    I use the Faker gem to generate times for each day.
    Though, I might remove `to_datetime` on line 23 since it's stored as the same object regardless.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -15,3 +17,18 @@ airport_names_and_codes.each do |item |
   Airport.create!(name: item.first, code: item.last)
 end
 puts 'created airports'
+
+31.times do |day|
+  current_datetime =
+    Faker::Time.between_dates(from: Date.today + (day - 1), to: Date.today + day).to_datetime
+
+  airport_names_and_codes.permutation(2) do |departure, arrival|
+    Flight.create!(
+      date: current_datetime,
+      departure_airport: Airport.find_by(code: departure),
+      arrival_airport: Airport.find_by(code: arrival),
+      duration: rand(45..320)
+    )
+  end
+end
+puts 'created flights'
