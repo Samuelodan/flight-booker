b0329ab7943088292f301e3c43121cb65f447fb8
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-03-21 00:03:49 +0100

    feat(Flight): write `#formatted_date` method

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+
+  def formatted_date
+    date.strftime('%e %b %Y')
+  end
 end
