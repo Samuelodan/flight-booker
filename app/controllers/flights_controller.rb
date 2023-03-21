bad50fa03757bfd05a2cdc03b5bbf00c2a3a5bb9
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-03-21 23:09:23 +0100

    feat(Flight): add more variables to hold queried flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,11 @@ class FlightsController < ApplicationController
   def index
     @flight_dates = Flight.all.map { |f| [f.formatted_date ,f.date.to_date ] }.uniq
     @airport_options = Airport.all.map { |ap| [ap.name, ap.code] }
+    departure_airport = Airport.find_by(code: params[:departure_airport])
+    arrival_airport = Airport.find_by(code: params[:arrival_airport])
+    @found_flights = Flight.where(
+      "date = ? AND departure_airport_id = ? AND arrival_airport_id = ?",
+      params[:flight_date], departure_airport.id, arrival_airport.id
+    )
   end
 end
