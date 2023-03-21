5f985220cca130bae437fb0cc4b975b3d312a79b
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-03-21 18:49:03 +0100

    feat(Flight): store airport options in `@airport_options`
    this is for the select form.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,6 @@
 class FlightsController < ApplicationController
   def index
     @flight_dates = Flight.all.map { |f| [f.formatted_date ,f.date.to_date ] }.uniq
+    @airport_options = Airport.all.map { |ap| [ap.name, ap.code] }
   end
 end
