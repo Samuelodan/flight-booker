4bb87f11973624fb5d4b86db55a929695365fc46
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-03-24 15:25:30 +0100

    feat(schema): create bookings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_17_222312) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_24_141527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
