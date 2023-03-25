cec0b19aab1fa8c01be0ff66cab4a626047259fd
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-03-25 23:05:19 +0100

    feat(Booking): write `#new` action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,8 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @flight = Flight.find(params[:selected_flight_id])
+    @passenger_count = params[:no_of_passengers].to_i
+    @passenger_count.times { @booking.passengers.build }
+  end
 end
