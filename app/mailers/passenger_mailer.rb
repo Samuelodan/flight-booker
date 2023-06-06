0da03dd8bca6ad1ef928deacc1204a6bf8a55328
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-06-06 21:29:20 +0100

    feat(PassengerMailer): define `ticket_email` method

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,3 +1,8 @@
 class PassengerMailer < ApplicationMailer
   default from: 'updates@example.com'
+
+  def ticket_email
+    @passenger = params[:passenger]
+    mail(to: @passenger.email, subject: "Thank you for booking a flight with us")
+  end
 end
